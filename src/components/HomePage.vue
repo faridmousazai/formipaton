bb3b64bfacca05f72cfb83ad8fe645a768d5fed8
farid <farid@aden.com>
2022-09-01 14:30:34 +0200

    farmipaton v0.8

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -12,15 +12,40 @@
 
     <ul class="menu__box">
       <li><a class="menu__item" href="#">Home</a></li>
-      <li>
-        <a class="menu__item" href="#">About</a>
+      <li class="menu__item">
+        <a class="item" href="#">Recettes</a>
         <ul class="submenu__box">
-          <li><a class="submenu__item" href="#">A1</a></li>
-          <li><a class="submenu__item" href="#">A2</a></li>
-          <li><a class="submenu__item" href="#">A3</a></li>
+          <li>
+            <a class="submenu__item" href="#"><span class="icon">🍸</span> Aperitifs </a>
+          </li>
+          <li>
+            <a class="submenu__item" href="#"> <span class="icon">🥗</span> Entrees</a>
+          </li>
+          <li>
+            <a class="submenu__item" href="#"><span class="icon">🍝</span> Plats</a>
+          </li>
+          <li>
+            <a class="submenu__item" href="#"><span class="icon">🍨</span> Desserts</a>
+          </li>
         </ul>
       </li>
-      <li><a class="menu__item" href="#">Contact</a></li>
+      <li class="menu__item">
+        <a class="item" href="#">Recettes de saison</a>
+        <ul class="submenu__box">
+          <li>
+            <a class="submenu__item" href="#"><span class="icon">🌷</span> Printemps</a>
+          </li>
+          <li>
+            <a class="submenu__item" href="#"><span class="icon">🏖</span> Eté</a>
+          </li>
+          <li>
+            <a class="submenu__item" href="#"><span class="icon">🍁</span> Automne</a></li>
+          <li>
+            <a class="submenu__item" href="#"><span class="icon">🗻</span> Hiver</a>
+          </li>
+        </ul>
+      </li>
+
       <li><a class="menu__item" href="#">Twitter</a></li>
     </ul>
   </div>
@@ -44,8 +69,7 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-  .submenu__box{ display: none}
-  .menu__item:hover ~ .submenu__box { display: block}
+ 
   body{
     background-color: rgba(20, 18, 18, 0.883);
     height: 100vh;
@@ -101,7 +125,7 @@ border-radius: 10px;
   content: '';
   top: 8px;
 }
-.menu__box {
+.menu__box{
   display: block;
   position: fixed;
   top: 0;
@@ -126,6 +150,38 @@ border-radius: 10px;
   transition-duration: .25s;
 }
 .menu__item:hover {
-  background-color: #7997a3;
+  background-color:rgb(192, 205, 204);
 }
+.submenu__box{ 
+  display: none;
+  width: 15vw;
+  z-index: 1;
+  opacity: 0.5rem;
+  list-style: none;
+  color: black;
+  
+}
+.submenu__box li{
+  width:100%;
+  position: relative;
+}
+.submenu__box li span{
+  width: 20%;
+  position: absolute;
+  left: 0;
+} 
+.submenu__item, .item{
+  color: #374141;
+  text-decoration: none;
+}
+.submenu__item{
+  color:#6a787d;
+  list-style:square;
+  font-size: 13px;
+}
+.icon{
+  font-size: 2em;
+}
+  .menu__item:hover .submenu__box { display: block}
+
 </style>
