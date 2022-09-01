9a040738d122f801a170030e353697f995fb4e87
Breizhpunisher53 <toto@toto.fr>
2022-09-01 14:33:35 +0200

    farmipaton v0.9

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,8 @@
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
   </nav>
-  <router-view/>
   <HomePage msg="Welcome to FARMIPATON"/>
+  <router-view/>
   
 </template>
 
