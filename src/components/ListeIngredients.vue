0d28ad5a31a893c62d3b54834f9014266867bfbc
Breizhpunisher53 <toto@toto.fr>
2022-09-13 11:35:11 +0200

    p

---
+++ src/components/ListeIngredients.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+    <div style="display: flex; justify-content: space-around" v-for="id_association in ingredients" :key="id_association">
+        <div class="case">{{ ingredient_from_rhi(id_association)?.fields.Name}} 
+        {{ rhi(id_association)?.fields.Quantity }} 
+        {{ rhi(id_association)?.fields.Unit}}</div> 
+       </div>
+    </div>
+</template>
+<script>
+    export default{
+        name: 'ListeIngredients',
+        props: {
+            ingredients: {type: Array}
+        },
+        computed: {
+            rhi() { return id_association => {return this.$store.getters['recette/getRecetteHasIngredientFromId'](id_association)}},
+            ingredient_from_rhi() {
+                return (id_association) => {
+                    let rhi = this.$store.getters['recette/getRecetteHasIngredientFromId'](id_association)
+                    let id = rhi?.fields.Ingredient[0]
+                    return this.$store.getters['ingredient/getIngredientFromId'](id)
+                
+                }
+            }
+        }
+    }
+</script>
+<style scoped>
+    
+  .case{
+   border: 1px solid black;
+   height: 20vh;
+   border-collapse: collapse;
+   width: 33%;
+    }
+</style>
--- src/views/Recettes.vue
+++ src/views/Recettes.vue
@@ -1,14 +1,14 @@
 
   <template>
     <div class="main">
-       <h1> {{name}} </h1>
+       <h1> {{recette_en_cours.name}} </h1>
        <div class="recette">
         <div class="image">
        <img src="https://fr.chatelaine.com/wp-content/uploads/2021/06/MuffinsRhubarbeOrange_580x434.jpg" alt="ça à l'air trop bon!">
      </div>
      <div class="texte">
        <h3>Description</h3>
-               {{description}} 
+               {{recette_en_cours.description}} 
         </div>
      </div>
      <br>
@@ -17,58 +17,35 @@
         <br>
         <br>
         <br>
-    <div class="tableau">
-       <div style="display: flex; justify-content: space-around" v-for="id_association in associations_avec_ingredients" :key="id_association">
-        <div class="case">{{ ingredient_from_rhi(id_association).fields.Name}}</div> 
-        <div class="case" >{{ rhi(id_association).fields.Quantity}}</div> 
-        <div class="case">{{ rhi(id_association).fields.Unit}}</div> 
-       </div>
+      <ListeIngredients :ingredients="recette_en_cours.associations_avec_ingredients" />
+       
       </div>
-      </div>    
+         
 </template>
 
 <script>
+   import ListeIngredients from "@/components/ListeIngredients.vue"
 export default {
+   components: {
+      ListeIngredients
+   },
    computed: {
-      rhi() { return id_association => {return this.$store.getters['recette/getRecetteHasIngredientFromId'](id_association)}},
-      ingredient_from_rhi() {
-         return (id_association) => {
-         let rhi = this.$store.getters['recette/getRecetteHasIngredientFromId'](id_association)
-         let id = rhi.fields.Ingredient[0]
-         return this.$store.getters['ingredient/getIngredientFromId'](id)
-      }
-      }
+      recette_en_cours() { return this.$store.getters['recette/getRecetteEnCours']},
    },
    created() {
-      const options = {headers: new Headers({
-        'Authorization': 'Bearer '+process.env.VUE_APP_AIRTABLE_APITOKEN, 
-        
-    })};
-      fetch("https://api.airtable.com/v0/appT0bvntx0RS1M8p/Recette/rectxIKMHXVmxzwyE", 
-      options)
-      .then(data => data.json())
-      .then(data => {
-         console.log(data.fields)
-         this.description = data.fields.Description
-         this.name = data.fields.Name
-         this.associations_avec_ingredients = data.fields.Recette_has_Ingredient
-      })
+     
+      this.$store.dispatch("recette/load_recette")
       this.$store.dispatch("recette/load_recette_has_ingredients")
       this.$store.dispatch("ingredient/load_ingredient")
-
-      
+     
    },
  data() {
    return {
-      description: "Lorem ipsum dolor sit amet,consectetur adipiscing elit. Aliquam lacinia ante lacus, a blandit orci varius vitae. Mauris pretium iaculis aliquam. Donec eget tellus tempor, interdum tellus ac, interdum augue. Nullam tincidunt malesuada placerat. Nulla blandit ante id fringilla ornare.In quis velit varius, condimentum augue id, efficitur odio. Sed faucibus metus non tellus feugiat efficitur. Sed elementum lacus a diam aliquam ullamcorper.Nullam in bibendum nisl. Integer mi erat, euismod eget ligula vitae, ornare fringilla quam.",
       recette_has_ingredient: [],
       ingredientNom :"",
-      associations_avec_ingredients: []
 }
 }   
 } 
- 
-
 </script>
 <style scoped>
   .recette{
@@ -92,13 +69,6 @@ export default {
   border: 1px solid black;
   margin: auto;
    }
-  .case{
-   border: 1px solid black;
-   height: 20vh;
-   border-collapse: collapse;
-   width: 33%;
-    }
  
-  
-  
+    
 </style>
