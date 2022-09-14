020b3fe39f7f6c78900c27c04112727b87ed46a9
WebdevMichel53 <webdevmichel53@gmail.com>
2022-09-14 12:32:42 +0200

    creation pages acceuil admin et editeur , modif appmenu

--- src/components/AppMenu.vue
+++ src/components/AppMenu.vue
@@ -1,41 +1,52 @@
 <template>
-   
-    <div class="">
-      <input id="menu__toggle" type="checkbox" />
+
+  <div class="">
+    <input id="menu__toggle" type="checkbox" />
     <label class="menu__btn" for="menu__toggle">
       <span></span>
     </label>
 
     <ul class="menu__box">
 
-      <li><router-link to="/" class="menu__item">Home</router-link></li>
-      <li><router-link to="/createaccount" class="menu__item">Creer un Account</router-link></li>
-      <li><router-link to="/login" class="menu__item">Login</router-link></li>
-      
+      <li>
+        <router-link to="/" class="menu__item">Home</router-link>
+      </li>
+      <li>
+        <router-link to="/createaccount" class="menu__item">Creer un Account</router-link>
+      </li>
+      <li>
+        <router-link to="/login" class="menu__item">Login</router-link>
+      </li>
+      <li>
+        <router-link to="/AcceuilAdmin" class="menu__item">Acceuil Administrateur</router-link>
+      </li>
+      <li>
+        <router-link to="/AcceuilEditor" class="menu__item">Acceuil Editeur</router-link>
+      </li>
+      <li>
+        <router-link to="/Recettes" class="menu__item">Recettes</router-link>
+      </li>
 
-      <li><router-link to="/Recettes" class="menu__item">Recettes</router-link></li>
-      <li><router-link to="/CreateRecette" class="menu__item">Creation Recette</router-link></li>
-      <li><router-link to="/DeleteRecette" class="menu__item">Effacer Recette</router-link></li>
-      <li><router-link to="/EditRecette" class="menu__item">Modifier Recette</router-link></li>
     </ul>
   </div>
-   
-    
+
+
 </template>
 <script>
-    export default {
-        name: "AppMenu"
-    }
+export default {
+  name: "AppMenu"
+}
 </script>
 <style scoped>
-    
-#menu__toggle:checked + .menu__btn > span::after {
+#menu__toggle:checked+.menu__btn>span::after {
   top: 0;
   transform: rotate(90deg);
 }
-#menu__toggle:checked ~ .menu__box {
+
+#menu__toggle:checked~.menu__box {
   left: 0 !important;
 }
+
 .menu__btn {
   position: absolute;
   top: 20px;
@@ -45,9 +56,10 @@
   cursor: pointer;
   z-index: 1;
 }
-.menu__btn > span,
-.menu__btn > span::before,
-.menu__btn > span::after {
+
+.menu__btn>span,
+.menu__btn>span::before,
+.menu__btn>span::after {
   display: block;
   position: absolute;
   width: 100%;
@@ -55,15 +67,18 @@
   background-color: #e94848;
   transition-duration: .25s;
 }
-.menu__btn > span::before {
+
+.menu__btn>span::before {
   content: '';
   top: -8px;
 }
-.menu__btn > span::after {
+
+.menu__btn>span::after {
   content: '';
   top: 8px;
 }
-.menu__box{
+
+.menu__box {
   display: block;
   position: fixed;
   top: 0;
@@ -77,6 +92,7 @@
   box-shadow: 2px 2px 6px rgba(197, 214, 211, 0.4);
   transition-duration: .25s;
 }
+
 .menu__item {
   display: block;
   padding: 12px 24px;
@@ -87,35 +103,40 @@
   text-decoration: none;
   transition-duration: .25s;
 }
+
 .menu__item:hover {
-  background-color:rgb(192, 205, 204);
+  background-color: rgb(192, 205, 204);
 }
-.submenu__box{ 
+
+.submenu__box {
   display: none;
   width: 15vw;
   z-index: 1;
   opacity: 0.5rem;
   list-style: none;
   color: black;
-  
+
 }
-.submenu__box li{
-  width:100%;
+
+.submenu__box li {
+  width: 100%;
   position: relative;
 }
-.submenu__box li span{
+
+.submenu__box li span {
   width: 20%;
   position: absolute;
   left: 0;
-} 
+}
 
 
 
-  .menu__item:hover .submenu__box { display: block}
+.menu__item:hover .submenu__box {
+  display: block
+}
 
-  #menu__toggle {
-    display: none !important;
-    /* visibility: hidden; */
-  }
-
+#menu__toggle {
+  display: none !important;
+  /* visibility: hidden; */
+}
 </style>
